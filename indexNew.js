aca8f8e5b792f7a6f165620e9b9e2c3000918b7e
Neha Yadav <86068532+NehaYadav903@users.noreply.github.com>
2021-08-16 00:21:31 -0400

    Create indexNew.js

---
+++ indexNew.js
@@ -0,0 +1,26 @@
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logTaskState: function() {
+      console.log(`${this.title} has${this.complete ? " ": " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  }
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logTaskState();
+task1.markCompleted();
+task1.logTaskState();
